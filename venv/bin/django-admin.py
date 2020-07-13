3c5f5047c953e85faf5ab1a45c6250ed1a714d35
zhujiaxu <zjx.public@gmail.com>
2020-07-12 20:40:54 -0400

    Refactored all files

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jiaxuzhu/Desktop/Study/Programming/Python/AirQuality/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
